8efbe3855b56979eae66dc60c242bd618c115969
Dio chang <jingyang0309@gmail.com>
2023-05-19 21:37:07 +0800

    feat:關ts

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
-import GlobalStyle from "./themes/GlobalStyle";
-import theme from './themes/theme';
+import GlobalStyle from "./themes/GlobalStyle.js";
+import theme from './themes/theme.js';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
